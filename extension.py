61a12ac4f090ad1037b857b857af4f0dc931eaf5
shannel890 <shannelkirui739@gmail.com>
2025-06-12 15:58:21 +0300

    modified extension.py

--- extension.py
+++ extension.py
@@ -1,3 +1,22 @@
+"""Flask extensions module."""
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_security import Security
+from flask_wtf.csrf import CSRFProtect
+from flask_mail import Mail
+from flask_babel import Babel
+from apscheduler.schedulers.background import BackgroundScheduler
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask_migrate import Migrate
 
+# Initialize Flask extensions
 db = SQLAlchemy()
+login_manager = LoginManager()
+security = Security()
+csrf = CSRFProtect()
+mail = Mail()
+babel = Babel()
+scheduler = BackgroundScheduler()
+limiter = Limiter(key_func=get_remote_address)
+migrate = Migrate()
